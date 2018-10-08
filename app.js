129d1910de68617e2cffb8e889461b6aa273c1f0
Skybbles // L5474 <skybluehexapon@outlook.com>
2018-10-07 21:27:11 -0400

    Decreased data rate interval, increased deadzone. Displays angle in degrees, distance from center, and 8-point direction in Unicode arrows.

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const xinput = require("./xinputjs-master");
 const logupd = require("log-update");
+const padEnd = require("lodash.padend");
+
+var dirSym = "";  
+
+var combi = [64, 270, 31, 90];
 
 [0]
 .filter(n => xinput.IsConnected(n))
 .map(n => xinput.WrapController(n, {
-	interval: 20,
+	interval: 15,
 	deadzone: {
-		x: 0.15,
-		y: 0.15
+		x: 0.18,
+		y: 0.18
 	},
 	holdtime: 500
 }))
@@ -22,8 +27,22 @@ const logupd = require("log-update");
     if(input == "rightstick") {
       x = data.x * 100;
       y = data.y * 100;
-      angle = Math.atan2(-x, -y) * 180 / Math.PI + 180;
-      logupd(Math.floor(angle) + '°');
+      angle = Math.floor(Math.atan2(-x, -y) * 180 / Math.PI + 180);
+      distance = Math.floor(Math.sqrt((x * x) + (y * y)));
+
+      if(337.5 < angle || angle < 22.5) { dirSym = "↑";
+      } else if(22.5 < angle && angle < 67.5) { dirSym = "↗";
+      } else if(67.5 < angle && angle < 112.5) { dirSym = "→";
+      } else if(112.5 < angle && angle < 157.5) { dirSym = "↘";
+      } else if(157.5 < angle && angle < 202.5) { dirSym = "↓";
+      } else if(202.5 < angle && angle < 247.5) { dirSym = "↙";
+      } else if(247.5 < angle && angle < 292.5) { dirSym = "←";
+      } else if(292.5 < angle && angle < 337.5) { dirSym = "↖"; }
+
+      if(distance < 10) dirSym = "+";
+
+      logupd(`${padEnd(angle, 3)}° ${padEnd(distance, 3)} ${dirSym}`);
+      //console.log(dirSym);
     }
 	});
 });
