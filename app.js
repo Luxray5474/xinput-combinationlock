433dc2b15f0143d1f9cef6cbd4af0f8e700838a7
Skybbles // L5474 <skybluehexapon@outlook.com>
2018-10-07 22:49:13 -0400

    Added **crude** passkey/pass combination system, consolidated parts of code

--- app.js
+++ app.js
@@ -4,28 +4,24 @@ const padEnd = require("lodash.padend");
 
 var dirSym = "";  
 
-var combi = [64, 270, 31, 90];
+var combi = [64, 270, 31, 90, 44, 85, 67, 300]; //pass combination
+var combiPosition = 0;
+
+var match = false;
 
 [0]
 .filter(n => xinput.IsConnected(n))
-.map(n => xinput.WrapController(n, {
-	interval: 15,
-	deadzone: {
-		x: 0.18,
-		y: 0.18
-	},
-	holdtime: 500
-}))
+.map(n => xinput.WrapController(n, { interval: 15, deadzone: { x: 0.18, y: 0.18 }, holdtime: 500 }))
 .forEach(gamepad => {
   var gamepadID = gamepad.deviceNumber;
   
   gamepad.addListener("connection-changed", (isConnected) => {
-		console.log(isConnected ? "Controller Connected" : "Controller Disconnected");
+    console.log(isConnected ? "Controller Connected" : "Controller Disconnected");
 	});
   
   gamepad.addListener("analog-input", (input, data) => { //what's beautiful about his wrapper is that it outputs both axes simultaneously
     if(input == "rightstick") {
-      x = data.x * 100;
+      x = data.x * 100; 
       y = data.y * 100;
       angle = Math.floor(Math.atan2(-x, -y) * 180 / Math.PI + 180);
       distance = Math.floor(Math.sqrt((x * x) + (y * y)));
@@ -41,8 +37,14 @@ var combi = [64, 270, 31, 90];
 
       if(distance < 10) dirSym = "+";
 
-      logupd(`${padEnd(angle, 3)}° ${padEnd(distance, 3)} ${dirSym}`);
-      //console.log(dirSym);
+      if(angle == combi[combiPosition]) { 
+        combiPosition++;
+          match = true;
+      }
+      
+      logupd(`${padEnd(angle, 3)}° | ${padEnd(distance, 3)} | ${dirSym} | ${combiPosition + 1} | ` + (match ? "🔓 Tchik!" : "🔒"));
+
+      if(combiPosition == combiPosition.length) {} else match = false;
     }
 	});
 });
